b42ccde68aefe2e1562d0bfce6c155227b65e11d
Shang-Rung Wang <ronnywang@gmail.com>
2011-11-05 20:38:34 +0800

    增加單一看版頁

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
 include(__DIR__ . '/init.php');
 
 Pix_Session::setCore('cookie', array('secret' => '60bf4e4009d422ab9322f1d4db48b02a'));
+Pix_Controller::addDispatcher(function($url){
+    list(, $contoller, $action) = explode('/', $url, 3);
+    switch ($contoller) {
+    case 'board':
+	return array('index', 'board');
+    }
+    return null;
+});
 Pix_Controller::dispatch(__DIR__ . '/webdata');
--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -5,4 +5,14 @@ class IndexController extends Pix_Controller
     public function indexAction()
     {
     }
+
+    public function boardAction()
+    {
+	list(, /*board*/, $board) = explode('/', $this->getURI());
+
+	if (!RankData::search(array('board' => $board))->first()) {
+	    throw new Exception('404');
+	}
+	$this->view->board = $board;
+    }
 }
