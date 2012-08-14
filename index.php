ad3fbb8f4d18b20355b17cd854fc9e36920456db
Ronny Wang <ronnywang@gmail.com>
2012-08-14 23:55:21 +0800

    remove unused Pix_Session

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 include(__DIR__ . '/init.php');
 
-Pix_Session::setCore('cookie', array('secret' => '60bf4e4009d422ab9322f1d4db48b02a'));
 Pix_Controller::addDispatcher(function($url){
     list(, $contoller, $action) = explode('/', $url, 3);
     switch ($contoller) {
