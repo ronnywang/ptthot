6a33ce5b1724093aa1a23045f9a8fb42a0d56ac8
Ronny Wang <ronnywang@gmail.com>
2013-01-15 15:34:35 +0800

    error when no DATABASE_URL

--- init.php
+++ init.php
@@ -13,20 +13,22 @@ if (file_exists(__DIR__ . '/webdata/setting.php')) {
 define('MESSAGE_SECRET', getenv('MESSAGE_SECRET'));
 date_default_timezone_set('Asia/Taipei');
 
-if (getenv('DATABASE_URL')) {
-    if (preg_match('#postgres://([^:]*):([^@]*)@([^/]*)/(.*)#', strval(getenv('DATABASE_URL')), $matches)) {
-	$user = $matches[1];
-	$pass = $matches[2];
-	$host = $matches[3];
-	$dbname = $matches[4];
-	Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_PgSQL(array('user' => $user, 'password' => $pass, 'host' => $host, 'dbname' => $dbname)));
-    } else if (preg_match('#mysql://([^:]*):([^@]*)@([^/]*)/(.*)#', strval(getenv('DATABASE_URL')), $matches)) {
-        $link = new mysqli;
-        $link->connect($matches[3], $matches[1], $matches[2]);
-        $link->select_db($matches[4]);
-        $link->set_charset('utf8');
-        Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_Mysqli($link));
-    }
+if (!getenv('DATABASE_URL')) {
+    die('need DATABASE_URL');
+}
+
+if (preg_match('#postgres://([^:]*):([^@]*)@([^/]*)/(.*)#', strval(getenv('DATABASE_URL')), $matches)) {
+    $user = $matches[1];
+    $pass = $matches[2];
+    $host = $matches[3];
+    $dbname = $matches[4];
+    Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_PgSQL(array('user' => $user, 'password' => $pass, 'host' => $host, 'dbname' => $dbname)));
+} else if (preg_match('#mysql://([^:]*):([^@]*)@([^/]*)/(.*)#', strval(getenv('DATABASE_URL')), $matches)) {
+    $link = new mysqli;
+    $link->connect($matches[3], $matches[1], $matches[2]);
+    $link->select_db($matches[4]);
+    $link->set_charset('utf8');
+    Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_Mysqli($link));
 }
 
 Pix_Controller::addCommonHelpers();
