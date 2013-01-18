bed913a3e61d13f5002bf9179591f9c9921417a8
Ronny Wang <ronnywang@gmail.com>
2013-01-18 16:39:13 +0800

    分開 putStream & putFile

--- webdata/models/DropboxLib.php
+++ webdata/models/DropboxLib.php
@@ -4,6 +4,12 @@ class DropboxLib
 {
     public function putFile($file, $target)
     {
+        $stream = fopen($file, 'r');
+        self::putStream($stream, $target);
+    }
+
+    public function putStream($stream, $target)
+    {
         $storage = new \Dropbox\OAuth\Storage\ArrayStorage();
         $config = new StdClass;
         $config->oauth_token = getenv('DROPBOX_ACCESS_KEY');
@@ -11,7 +17,6 @@ class DropboxLib
         $storage->set($config, 'access_token');
         $OAuth = new \Dropbox\OAuth\Consumer\Curl(getenv('DROPBOX_KEY'), getenv('DROPBOX_SECRET'), $storage, $callback);
         $dropbox = new \Dropbox\API($OAuth);
-        $stream = fopen($file, 'r');
         $result = $dropbox->putStream($stream, $target);
     }
 }
