c411d417179764c509f5ca58888fc91134f1bb5a
Ronny Wang <ronnywang@gmail.com>
2013-01-21 09:37:53 +0800

    增加 TitleHistory 更新的動作

--- webdata/models/TitleHistory.php
+++ webdata/models/TitleHistory.php
@@ -1,14 +1,55 @@
 <?php
 
+class TitleHistoryRow extends Pix_Table_Row
+{
+    public function postSave()
+    {
+        TitleHistory::clearCache($this->board);
+    }
+}
+
 class TitleHistory extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'titlehistory';
         $this->_primary = array('board', 'time');
+        $this->_rowClass = 'TitleHistoryRow';
 
         $this->_columns['board'] = array('type' => 'varchar', 'size' => 32);
         $this->_columns['time'] = array('type' => 'int');
         $this->_columns['title'] = array('type' => 'text');
+    }
+
+    protected static $_latest_cache = array();
+
+    public static function clearCache($board)
+    {
+        unset(self::$_latest_cache[$board]);
+    }
+
+    public static function updateTitle($board, $time, $title)
+    {
+        $titlerow = self::getLatestTitle($board);
+        if ($titlerow and $title == $titlerow->title) {
+            return;
+        }
+        if ($time < $titlerow->time) {
+            return;
+        }
+        TitleHistory::insert(array(
+            'board' => $board,
+            'time' => $time,
+            'title' => $title,
+        ));
+    }
+
+    public static function getLatestTitle($board)
+    {
+        if (!array_key_exists($board, self::$_latest_cache)) {
+            self::$_latest_cache[$board] = TitleHistory::search(array('board' => $board))->max('time');
+        }
+
+        return self::$_latest_cache[$board];
     }
 }
