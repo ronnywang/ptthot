ed9381ece12f149565dc4c1232be4c28326600ad
Ronny Wang <ronnywang@gmail.com>
2014-12-08 18:19:23 +0800

    加上 /index/health

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -20,4 +20,16 @@ class IndexController extends Pix_Controller
     {
         return $this->redirect('/board/' . urlencode($_GET['q']));
     }
+
+    public function healthAction()
+    {
+        $latest_hots = json_decode(KeyValue::get('latest_hot'));
+        $latesttime = $latest_hots->time;
+
+        if (time() - $latesttime > 3600) {
+            echo 'error from ' . date('c', $latesttime);
+        } else {
+            echo 'ok';
+        }
+    }
 }
